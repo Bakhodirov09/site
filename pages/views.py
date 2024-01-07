a4cbd04b415b3a49fc6d24e05bf495eb97685235
Bahodirov Nurulloh <bloghash01@gmail.com>
2024-01-07 19:07:46 +0500

    first_commit

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
 
 def view_portfolio(request):
-    return render(request, 'games.html')
+    data = request.GET
+    num1 = int(data.get("num1", 0))
+    num2 = int(data.get("num2", 0))
+    amal = data.get("amal", "+")
+    result = None
+    if amal == "+":
+        result = num1 + num2
+    elif amal == "-":
+        result = num1 - num2
+    elif amal == "*":
+        result = num1 * num2
+    elif amal == "/":
+        result = num1 / num2
+    else:
+        result = "Kechirasiz + - * / Amallaridan Birini Kiriting!"
+    context = {
+        "result": result
+    }
+    return render(request, 'games.html', context)
